5f04f48bdc98606730b217522a45bb1f3d31fc49
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2025-10-30 17:56:03 +0200

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="text-2xl text-blue-600">HomePage</div>;
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./assets/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  plugins: [],
+};
