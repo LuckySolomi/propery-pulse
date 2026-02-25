75f05339942341bd1c1bdada121d06987e636b9f
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-02-26 01:53:59 +0200

    Connects sign out button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -206,6 +206,10 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setProfileMenuOpen(false);
+                        signOut();
+                      }}
                     >
                       Sign Out
                     </button>
