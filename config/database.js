674cc24ac14e0e5e39db3be5861de3a62cead678
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-03-14 00:07:30 +0200

    Fixes grammar mistakes

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
-let conected = false;
+let connected = false;
 
 const connectDB = async () => {
   mongoose.set("strictQuery", true);
 
-  if (conected) {
+  if (connected) {
     console.log("MongoDB is connected ");
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    conected = true;
+    connected = true;
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
