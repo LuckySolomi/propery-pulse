8be7d3dca83146480fd0caa7466d6bffe2fbc8f6
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-03-21 00:10:33 +0200

    Creates sidebar

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyImages from "@/components/PropertyImages";
+import BookmarkButton from "@/components/BookmarkButton";
+import ShareButtons from "@/components/ShareButtons";
+import PropertyContactForm from "@/components/PropertyContactForm";
 import connectDB from "@/config/database";
 import Property from "@/models/property";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 import { notFound } from "next/navigation";
 import { convertToSerializableObject } from "@/utils/convertToObject";
 
@@ -41,6 +44,11 @@ const PropertyPage = async ({ params }) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
