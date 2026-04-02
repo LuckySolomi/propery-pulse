365b58c439065487d668d7017b1de87b60521863
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-04-02 20:26:40 +0300

    Adds global context

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import QueryProvider from "@/components/QueryProvider";
+import { GlobalProvider } from "@/context/GlobalContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "@/assets/styles/globals.css";
@@ -16,14 +17,16 @@ const MainLayout = ({ children }) => {
   return (
     <QueryProvider>
       <AuthProvider>
-        <html lang="en">
-          <body>
-            <Navbar />
-            <main> {children}</main>
-            <Footer />
-            <ToastContainer position="top-right" autoClose={3000} />
-          </body>
-        </html>
+        <GlobalProvider>
+          <html lang="en">
+            <body>
+              <Navbar />
+              <main> {children}</main>
+              <Footer />
+              <ToastContainer position="top-right" autoClose={3000} />
+            </body>
+          </html>
+        </GlobalProvider>
       </AuthProvider>
     </QueryProvider>
   );
